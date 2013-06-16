42ea2fb47768853e72c0e24f7755046a1b4f6793
wilbertom <wilbertomorales777@gmail.com>
2013-06-15 20:39:33 -0500

    Static data in json format and csv to json util

---
+++ csv2json.py
@@ -0,0 +1,13 @@
+import csv, json
+def csv2json(filename, *fieldnames):
+    f = open( filename, 'r' )
+    reader = csv.DictReader( f, fieldnames = fieldnames  )
+    reader.next()
+    out = json.dumps( [ row for row in reader ] )
+    new_file = open(f.name.replace('csv', 'json'), 'w')
+    new_file.write(out)
+    f.close
+    return new_file
+
+if __name__ == '__main__':
+    csv2json('data/counties.csv', 'Geography', 'USA', 'population', 'unemployment rate', 'high school',	'high school %', 'GDP per capita $')
